c0f6f5eef4842da02aa8386c4c52e1302bb88d32
Olivier HENRY <reivilo2002@hotmail.com>
2018-03-23 07:08:35 +0100

    Started picked room validation process

--- src/app/providers/rooms.service.ts
+++ src/app/providers/rooms.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
 import { getConnection, Repository } from 'typeorm';
 import Room from '../../entity/Room';
+import * as os from 'os';
+import * as fs from 'fs';
+import { patch } from 'webdriver-js-extender';
 
 @Injectable()
 export class RoomsService {
 
   private repository: Repository<Room>;
   public rooms: Array<Room>;
-  public knowRooms:Array<string> = ['winamax', 'pokerstars', 'test'];
+  public knowRooms: Array<string> = ['winamax', 'pokerstars', 'test'];
 
   constructor() {
     this.repository = getConnection().getRepository(Room);
@@ -15,25 +18,48 @@ export class RoomsService {
   }
 
   private get = () => {
-     this.repository.find()
+    this.repository.find()
       .then(response => {
-        if(response.length){
+        if (response.length) {
           this.rooms = response;
-          
-          for(let room of this.rooms){
+
+          for (let room of this.rooms) {
             let i = this.knowRooms.indexOf(room.name.toLowerCase());
 
-            if(i >= 0){
-              this.knowRooms.splice(i,1);
+            if (i >= 0) {
+              this.knowRooms.splice(i, 1);
             }
           }
         }
       });
   }
 
-  public validateRoomFolder = (roomName: string, path: string): boolean => {
+  private addRoom = (roomName: string, path: string) => {
+
+  }
+
+  private validateRoomFolder = (roomName: string, path: string): boolean | Room => {
+    switch (roomName) {
+      case 'pokerstars':
+        return this.isValidPokerstarsPath(path);
+      case 'winamax':
+        return this.isValidWinamaxPath(path);
+      default:
+        return false;
+    }
+  }
+
+  private isValidPokerstarsPath = (path: string): boolean | Room => {
+    
+
 
     return false;
   }
+  private isValidWinamaxPath = (path: string): boolean | Room => {
+
+    return false;
+  }
+
+
 
 }
